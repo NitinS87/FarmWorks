10c182b5c9a9c0f674e8ff2f3604f3d195c0dbd3
Nitin Sharma <80587065+NitinS87@users.noreply.github.com>
2023-03-10 17:17:03 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -5,8 +5,8 @@ let reqInstance = axios.create({
   headers: {
     token: `${token}`,
   },
-  // baseURL: "https://farm-works-server.vercel.app/",
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://farm-works-server.vercel.app/",
+//   baseURL: "http://localhost:8000/",
 });
 
 export default reqInstance;
