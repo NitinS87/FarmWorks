cf80bad4f05c124b3b73edb59d50ad1b4d8c9a36
Nitin Sharma <80587065+NitinS87@users.noreply.github.com>
2023-10-11 12:46:16 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("Authorization");
 let reqInstance = axios.create({
   headers: {
-    token: `${token}`,
+    token: `Bearer ${token}`,
   },
   baseURL: "https://farm-works-server.vercel.app/",
 //   baseURL: "http://localhost:8000/",
